1ee47f1ac43baa06b628ebe28550e0e7b9198ad4
stevanoliveira <stevan.oliver@hotmail.com>
2022-08-10 17:41:03 -0300

    atualização de comandos

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -48,10 +48,12 @@ describe('Central de Atendimento ao Cliente TAT', function() {
        
     })
 
-    it('campo telefone continua vazio quando preenchido com valor não-numérico', function(){
-        cy.get('#phone')
-            .type('abcdefghij')
-            .should('have.value', '')
+    Cypress._.times(3, function() {
+        it.only('campo telefone continua vazio quando preenchido com valor não-numérico', function(){
+         cy.get('#phone')
+          .type('abcdefghij')
+          .should('have.value', '')
+       })
     })
 
     it('exibe a mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
